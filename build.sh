d0b5d46dcc296e337c1c583fe4495fda5487d822
AbdulMutakabbir <a_mutakabbir@yahoo.com>
2024-12-28 20:30:28 -0500

    script to build prod service

---
+++ build.sh
@@ -0,0 +1,11 @@
+# install packages
+npm install
+
+# fix package compatability issues
+npm audit fix
+
+# build the deployment files 
+npm run build
+
+# install the production server 
+npm install -g serve
