1d59b1acf53e5331f207735005a64a4223462bb8
AbdulMutakabbir <a_mutakabbir@yahoo.com>
2024-12-29 02:52:41 -0500

    fixed alignment of logo + code cleaning

--- src/components/PublicationTimelineView.jsx
+++ src/components/PublicationTimelineView.jsx
@@ -1,4 +1,4 @@
-import { Typography, Button } from '@mui/material';
+import { Button } from '@mui/material';
 import { Timeline, TimelineConnector, TimelineContent, TimelineDot, TimelineItem, TimelineOppositeContent, TimelineSeparator } from '@mui/lab';
 import StarsIcon from '@mui/icons-material/Stars';
 import CardPublication from "./CardPublication";
@@ -6,33 +6,20 @@ import CardPublication from "./CardPublication";
 const PublicationTimelineView = (props) => {
     return (
         <Timeline 
-            // sx={{
-            //     position: {
-            //         xl: 'alternate',   // Extra large screens (large desktops)
-            //         xs: 'left',   // Extra small screens (phones)
-            //         sm: 'right',   // Small screens (tablets)
-            //         md: 'alternate',   // Medium screens (laptops)
-            //         lg: 'alternate',   // Large screens (desktops)
-            //     },
-            // }}
-            // position="alternate"
-            position="right"
-            // sx={{
-            //     [`& .${timelineOppositeContentClasses.root}`]: {
-            //       flex: {md: 0.4, lg: 0.2 },
-            //     },
-            //   }}
+            position="left"
         >
             {props.publications.map((publication, index) => 
                 <TimelineItem
-                style={{
-                    display: 'flex',
-                }}
-                sx={{
-                    flexDirection: {
-                        sm: 'row',
-                        md: index%2===0 ? 'row-reverse' : 'row'
-                }}}
+                    style={{
+                        display: 'flex',
+                    }}
+                    sx={{
+                        position: index%2===0 ? 'left' : 'left',
+                        flexDirection: {
+                            sm: 'row',
+                            md: index%2===0 ? 'row-reverse' : 'row'
+                        }
+                    }}
                 >
                     <TimelineOppositeContent 
                         alignContent='center'
@@ -45,31 +32,15 @@ const PublicationTimelineView = (props) => {
                                 sm: 'none',   
                                 md: 'flow-root'
                             },
+                            textAlign: index%2===0 ? 'left' : 'right'
                         }}
                     >
                         <Button
+                                color='primary'
                             variant="rounded"
                             disableRipple
-                            sx={{
-                                borderRadius: 10,
-                                boxShadow: 10,
-                                bgcolor: "#ffffffea",
-                            }}
                         >
-                            <Typography 
-                                color='primary'
-                                sx={{
-                                    variant: {
-                                      xs: 'body2',   // Extra small screens (phones)
-                                      sm: 'h5',   // Small screens (tablets)
-                                      md: 'h4',   // Medium screens (laptops)
-                                      lg: 'h3',   // Large screens (desktops)
-                                      xl: 'h2',   // Extra large screens (large desktops)
-                                    },
-                                  }}
-                            >
-                                {publication.publisherShort} {publication.year}
-                            </Typography>
+                            <img src={publication.logo} height="100px" alt="logo"/>
                         </Button>
                     </TimelineOppositeContent>
                     <TimelineSeparator>
@@ -84,7 +55,10 @@ const PublicationTimelineView = (props) => {
                             flex: 1
                         }}
                     >
-                        <CardPublication publication={publication} />
+                        <CardPublication 
+                            disableImage
+                            publication={publication} 
+                        />
                     </TimelineContent>
                 </TimelineItem>
             )}
